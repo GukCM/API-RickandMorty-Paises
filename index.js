af703fde019fbb79e6050a1c2e7e5bb3bea26f1f
GukCM <gusavocod@gmail.com>
2022-12-13 16:47:52 -0600

    API RICK PAISES

---
+++ index.js
@@ -0,0 +1,61 @@
+import fetch from 'node-fetch';
+const apiRick = async (id) =>{
+    const res = await fetch(`https://rickandmortyapi.com/api/character/${id}`);
+    const data = await res.json();
+    const {name, species} = data;
+    console.log(
+        `Nombre: ${name}
+Especie: ${species}`);
+}    
+
+const apiCountry = async(lng) =>{
+    const res2 = await fetch(`https://restcountries.com/v3.1/lang/${lng}`);
+    const data2 = await res2.json();
+    const [info] = data2;
+    const {name:{common}} = info;
+    // // const {common} = name;
+    console.log(`País: ${common}`);
+}
+apiCountry('spa');
+apiRick(1);
+
+// // console.log("Hola");
+//     input: process.stdin,    
+//     output: process.stdout
+//   });
+//   //Devuelve string independientemente si ingresamos caracteres, numeros o signos
+//   readline.question('Ingresa el id del personaje que deseas: ', id => {
+    //     console.log(id);    
+        //     readline.close();
+        //   });
+        // fetch('https://rickandmortyapi.com/api/character/1')
+        // .then((info)=> info.json().then((res) => console.log(res)))
+        // .catch((err)=>{console.log(err)});
+        
+        
+        // fetch('https://rickandmortyapi.com/api/character/1')
+        // .then((info)=> info.json().then((res) =>{
+            //     const {name, species} = res;    
+            //     console.log(name,species);    
+                // } ))
+// .catch((err)=>{console.log(err)});
+
+
+// const apiCountry = async(name) =>{
+    //     const res2 = await fetch(`https://restcountries.com/v3.1/name/${name}`);    
+    //     const data2 = await res2.json();
+    //     console.log(data2);
+    //     const {fifa} = data2;
+    //     console.log(fifa);
+    // }
+    // apiCountry('mexico');
+    
+    
+    
+    
+    
+    // Object.keys(info).map(key =>{
+            // const readline = require('readline').createInterface({
+        //     const {name:{common}} = info;
+        //     console.log(common);
+        // })
